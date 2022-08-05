0cc6ed26239d23130ae7d8794658c2c3834fdbd8
fredd <freddypcarrion@gmail.com>
2022-08-05 11:39:20 -0500

    start of app

---
+++ ccus_app.py
@@ -0,0 +1,9 @@
+# Import Python Libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+# Desing of the app
+st.set_page_config(page_title="CCUS App")
+# Create title of web app
+st.title("CO2 Emissions App")
