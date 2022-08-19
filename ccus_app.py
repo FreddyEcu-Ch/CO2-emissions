65dfe017ad6f74c317313cfddac1936bae2d884c
fredd <freddypcarrion@gmail.com>
2022-08-19 11:10:44 -0500

    App updated.

--- ccus_app.py
+++ ccus_app.py
@@ -2,8 +2,29 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+from PIL import Image
+
+# from streamlit_option_menu import option_menu
 
 # Desing of the app
 st.set_page_config(page_title="CCUS app")
 # Create title of web app
 st.title("CO2 Emissions App")
+
+# List of options
+options = st.sidebar.radio(
+    "Choose your option",
+    ("Home", "Refineries data", "Thermal plants data", "Surface Facilities"),
+)
+
+if options == "Home":
+    st.header("""-**Objectives:** App for view the ccus potential: """)
+    st.header("""-**Methodology:**""")
+    # Load image
+    image = Image.open("resources/ccs.jpg")
+    st.image(image)
+    st.caption("*CCUS Framework*")
+    # Load video
+    video = open("resources/ccus.mp4", "rb")
+    st.video(video)
+    st.caption("*CCUS Value Chain*")
